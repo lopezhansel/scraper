c104926eff52cd1a5607b73676d5421a19acc98e
Hansel Lopez <lopezhansel@gmail.com>
2017-03-08 08:34:07 -0700

    Adding some types

--- src/Scraper.ts
+++ src/Scraper.ts
@@ -1,11 +1,18 @@
 import * as q from 'q'
 import * as rp from 'request-promise'
 import * as cheerio from 'cheerio'
-import * as  bluebird from 'bluebird'
+import * as bluebird from 'bluebird'
 
-export class Scraper {
+export interface options {
+    url : string;
+    howMany : number;
+    getUrl : (num) => string
+    bodyParser : ($ : CheerioStatic) => void
+}
 
-    static getPages(opts) {
+export default class Scraper {
+
+    static getPages(opts : options) {
         var promisesArr = []
         for (var int = 0; int <= opts.howMany - 1; int++) {
             promisesArr.push(Scraper.rpCheerio(opts.getUrl(int)).then(opts.bodyParser))
